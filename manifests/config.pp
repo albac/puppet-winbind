aed550087b9be0f0eb09dbeffce421198a88cb06
Alfredo Baldoceda <alfrds@gmail.com>
2015-06-04 19:00:58 -0700

    adding krb5config

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,6 +8,8 @@ class winbind::config (
   $realm           = $::winbind::realm
 ) {
 
+  $upcase_domain=upcase($realm)
+
   # Main samba config file
   file { 'smb.conf':
     name    => '/etc/samba/smb.conf',
@@ -17,12 +19,21 @@ class winbind::config (
     content => template($smb_template),
   }
 
+  # Authconfig
+  exec { 'authconfig-krb5':
+    command => "authconfig --disablecache --enablewinbind --enablewinbindauth --smbsecurity=ads --smbworkgroup=$domain --smbrealm=$upcase_domain --enablewinbindusedefaultdomain --winbindtemplatehomedir=/home/%U --winbindtemplateshell=/bin/bash --enablekrb5 --krb5realm=$upcase_domain --enablekrb5kdcdns --enablekrb5realmdns --enablelocauthorize --enablemkhomedir --enablepamaccess --updateall",
+    onlyif  => "wbinfo --own-domain | grep -v ${domain}",
+    path => $path,
+    require => File['smb.conf'],
+    
+  }
+
   # Add the machine to the UOB domain
   exec { 'add-to-domain':
     command => "net ads join -U ${domainadminuser}%${domainadminpw}",
     onlyif  => "wbinfo --own-domain | grep -v ${domain}",
     path    => '/bin:/usr/bin',
-    require => File['smb.conf'],
+    require => Exec['authconfig-krb5']
   }
 
 }
